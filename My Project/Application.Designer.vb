7271b6d8b550b3996274ccaedcde8a8428abc4f8
Caleb Azofeifa <caleb.lazofeifa2707@gmail.com>
2024-03-29 19:46:41 -0600

    Se añade el botón para insertar imagenes en la base de datos.

--- My Project/Application.Designer.vb
+++ My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.PRY_Preguntados.FrmMenuProfesor
+            Me.MainForm = Global.PRY_Preguntados.FrmPregunta
         End Sub
     End Class
 End Namespace
